198f1af01e8fc6086c22062d8a46260087afca91
Suman choudhary <sc108807@gmail.com>
2025-07-16 03:37:17 +0530

    updated code

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//Simple Java Calculator that performs addition and subtraction using user
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        double num1 = sc.nextDouble();
+        System.out.println("Enter second number: ");
+        double num2 = sc.nextDouble();
+        System.out.println("Enter operator: (+ or - ): ");
+        char operator = sc.next().charAt(0);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        double result;
+
+        if (operator == '+'){
+            result = num1 + num2;
         }
+        else if (operator == '-'){
+            result = num1 - num2;
+        }
+        else {
+            System.out.println("Invalid operator");
+            return;
+        }
+        System.out.println("Result: " + result);
     }
 }
